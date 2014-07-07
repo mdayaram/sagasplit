629407c4b3af67653873b549ba8c62ba88bdba53
Manoj Dayaram <noj@moovweb.com>
2014-07-07 01:15:08 -0700

    nicer output

--- run.rb
+++ run.rb
@@ -21,7 +21,9 @@ begin
   sagas = facebook.collect_sagas(day_links)
 
   sagas.each_pair do |author, ss|
-    puts "\n** #{author}'s Mini-Saga Collection **\n"
+    title = "#{author}'s Mini-Saga Collection"
+    puts "\n\n\n#{title}\n"
+    puts ("=" * title.length) + "\n"
     ss.sort.map do |day, s| #sorts by days
       puts "\nDay #{day}:"
       puts s.join("\n\n")
